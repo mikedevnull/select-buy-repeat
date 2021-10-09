0865c5e0253bb8f6e037d1615820e302ea68be7f
Michael Meier <michael-meier@bluesheep.de>
2021-10-09 16:49:34 +0200

    switch from cra to vite

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import "onsenui/css/onsenui.css";
+import "onsenui/css/onsen-css-components.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
