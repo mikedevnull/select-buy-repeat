d7c8c3b655cc0f46ea062c89efa4553a328ff8b5
Michael Meier <michael-meier@bluesheep.de>
2021-10-10 01:38:48 +0200

    add pwa sw support

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { registerSW } from "virtual:pwa-register";
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    console.log("refresh needed");
+    updateSW();
+  },
+  onOfflineReady() {
+    console.log("offline ready");
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,40 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()],
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: [
+        "favicon.svg",
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon.png",
+      ],
+      manifest: {
+        name: "Select Buy Repeat",
+        short_name: "SelectBuyRepeat",
+        description: "Yet another list thingy",
+        theme_color: "#ffc400",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
   base: "/select-buy-repeat/",
 });
