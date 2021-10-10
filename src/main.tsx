101e5fdade1bc4424ee8347a057905d57e3827f4
Michael Meier <michael-meier@bluesheep.de>
2021-10-10 22:31:49 +0200

    remove duplicate service worker setup

--- src/main.tsx
+++ src/main.tsx
@@ -4,16 +4,6 @@ import "./index.css";
 import App from "./App";
 import { registerSW } from "virtual:pwa-register";
 
-const updateSW = registerSW({
-  onNeedRefresh() {
-    console.log("refresh needed");
-    updateSW();
-  },
-  onOfflineReady() {
-    console.log("offline ready");
-  },
-});
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
